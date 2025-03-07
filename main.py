c2418bea2f02653721aa9bce1c57be3f0ed2d196
Simon Vargas <sjvargas01@gmail.com>
2025-03-07 18:13:14 -0500

    updates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "fe467a8e2f144eff93ee6bcc33d5543f"
+api_key = ""
 url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=" + api_key
 
 request = requests.get(url)
